aee94e0d3e04904abf6281cf79487dad2669862b
navgurukul <jai@navgurukul.org>
2020-08-15 19:56:35 +0530

    getting database git error

--- DFSL/settings.py
+++ DFSL/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'DFSL.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'django_user_db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'new_django_user_db.sqlite3'),
     }
 }
 
