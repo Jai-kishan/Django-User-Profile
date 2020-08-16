0e5c760cc14de0414ffa7fd4dc3d73f42cb32863
navgurukul <jai@navgurukul.org>
2020-08-16 17:49:21 +0530

    updated KaamKaj

--- kaamkaj/urls.py
+++ kaamkaj/urls.py
@@ -3,10 +3,7 @@ from .views import *
 
 
 urlpatterns =[
-	# path('signup/', signup_user, name='signup'),
 	path('', kaamkaj, name='kaamkaj'),
-	# path('logout/', logout_user, name='logout'),
-	# path('login/', login_user, name='login'),
 	path('create/', create_kaam, name='create_kaam'),
 	path('todos/', kaam_list, name='kaamkaj_list'),
 	path('todo/<int:todo_id>', kaamkaj_details, name='kaam_details'),
--- kaamkaj/views.py
+++ kaamkaj/views.py
@@ -19,43 +19,8 @@ def kaam_list(request):
 	todos  = KaamKaj.objects.filter(user=request.user, complete_date__isnull=True).order_by('-created')
 	return render(request,'kaamkaj/current_kaam.html',locals())
 
-# def signup_user(request):
-# 	if request.method == "GET":
-# 		return render(request,'todos/signup.html',{'form':UserCreationForm})
-# 	else:
-# 		if request.POST.get('password1') == request.POST.get('password2'):
-# 			try:
-# 				user = User.objects.create_user(username=request.POST.get('username'),password=request.POST.get('password1'))
-# 				user.save()
-# 				login(request,user)
-# 				return redirect('home')
-
-# 			except IntegrityError:
-# 				return render(request,'todos/signup.html',{'form':UserCreationForm, 'error':"That username has already been taken. Please choose a new username."})
-# 		else:
-# 			return render(request,'todos/signup.html',{'form':UserCreationForm, 'error':"Password didn't match"})
-		
-# @login_required
-# def logout_user(request):
-# 	if request.method == "POST":
-# 		logout(request)
-# 		return redirect('login')
-
-
-# def login_user(request):
-# 	if request.method == "GET":
-# 		return render(request,'todos/login.html',{'form':AuthenticationForm})
-# 	else:
-# 		user = authenticate(request,username=request.POST.get('username'), password=request.POST.get('password'))
-# 		if user is None:
-# 			return render(request,'todos/login.html',{'form':AuthenticationForm, 'error':"username and password didn't matched"})
-# 		else:
-# 			login(request,user)
-# 			return redirect('home')
-
 @login_required
 def create_kaam(request):
-	# import ipdb; ipdb.set_trace()
 	if request.method == 'GET':
 		return render(request,'kaamkaj/create_kaam.html', {'form':TodoForm})
 	else:
@@ -70,7 +35,6 @@ def create_kaam(request):
 
 @login_required
 def kaamkaj_details(request,todo_id):
-	# import ipdb;ipdb.set_trace()
 	todo = get_object_or_404(KaamKaj, id=todo_id, user=request.user)
 	if request.method == "GET":
 		form = TodoForm(instance=todo)
